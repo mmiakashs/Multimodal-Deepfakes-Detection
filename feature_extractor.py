0967b6d5bbbff3a8b974235ab218aa1a3cd366a1
Md Mofijul Islam (Akash) <akash.cse.du@gmail.com>
2020-03-20 22:45:49 -0400

    feature extractior added

--- feature_extractor.py
+++ feature_extractor.py
@@ -8,11 +8,14 @@ from PIL import Image
 from torch.utils.data import Dataset
 from torch.utils.data.dataloader import DataLoader
 from torchvision.transforms import transforms
+from torchvision import models
 
 from src.utils import config
 from src.dataloader.deep_fake_dataset import *
 
 data_dir_base_path = '/data/research_data/dfdc_train_data'
+embed_dir_base_path = '/data/research_data/dfdc_embed'
+
 rgb_transforms = transforms.Compose([
             transforms.Resize((224, 224)),
             transforms.ToTensor(),
@@ -21,7 +24,16 @@ rgb_transforms = transforms.Compose([
                 std=[0.229, 0.224, 0.225]
             )])
 
+feature_extractor = models.resnet50(pretrained=True)
 for dirname, _, filenames in os.walk(data_dir_base_path):
     for filename in filenames:
-        video = Video(f'{data_dir_base_path}/aagfhgtpmv.mp4', transforms=rgb_transforms)
-        frames = video.get_all_frames()[0].size()
+        video = Video(f'{data_dir_base_path}/{filename}', transforms=rgb_transforms)
+        seq, seq_len = video.get_all_frames()
+        embed = feature_extractor(seq)
+        filename = filename.split('.')[0]
+        torch.save(embed, f'{embed_dir_base_path}/{filename}.pt')
+        print('embedding is saved to: ',f'{embed_dir_base_path}/{filename}.pt')
+        print('embedding size', embed.size())
+        break
+
+print('feature_extraction_complete')
